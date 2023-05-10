b2f5ee5b4b2794854c189fee95ff3c45dc8ef846
mttble <matthew.beale1@hotmail.com>
2023-05-10 18:07:11 +0930

    edited to allow for exiting application

--- typeme.py
+++ typeme.py
@@ -5,29 +5,45 @@ import random
 class Typeme:
 
     def __init__(self):
-        input("Press Enter to start the typing speed test...")
+        input("Press Enter to start Typeme!!!")
         self.texts = open("texts.txt", "r").read().split("\n")
         self.reset()
 
         while self.running:
-            print()  # Add a line break
-            print(self.color_text(self.sample_text, "yellow"))
-            print()  # Add a line break
+            print()  
+            print(self.color_text(self.sample_text, "yellow")) #prints random sentence
+            print()  
             user_input = input("Type the above text: ")
+
 
             if user_input == self.sample_text:
                 self.running = False
                 self.save_results()
                 print(f"{self.color_text('Correct! Your typing speed: {:.2f} Words per minute'.format(self.speed), 'green')}")
                 print(f"{self.color_text(self.get_average_wpm(), 'blue')}")
-                input("Press Enter to start the typing speed test...")
-                self.texts = open("texts.txt", "r").read().split("\n")
-                self.reset()
+                while True:
+                    # need to allow user to exit application
+                    user_input = input("Press Enter to start or type 'exit' to quit...")
+                    if user_input.lower() == "exit":
+                        print("Exiting now...")
+                        exit()
+                    elif user_input == "":
+                        self.texts = open("texts.txt", "r").read().split("\n")
+                        self.reset()
+                        break
 
             else:
+                # not very DRY but need to be able to exit in both scenarios
                 print(f"{self.color_text('Incorrect. Try again.', 'red')}")
-                input("Press Enter to start the typing speed test...")
-                self.reset()
+                while True:
+                    user_input = input("Press Enter to start or type 'exit' to quit...")
+                    if user_input.lower() == "exit":
+                        print("Exiting now...")
+                        exit()
+                    elif user_input == "":
+                        self.texts = open("texts.txt", "r").read().split("\n")
+                        self.reset()
+                        break
 
 
     def reset(self):
@@ -45,10 +61,10 @@ class Typeme:
             speeds = []
             for line in f:
                 if line.startswith("Speed:"):
-                    speed = float(line.split(":")[1].split("WPM")[0].strip())
+                    speed = float(line.split(":")[1].split("WPM")[0].strip())#extract the numbers
                     speeds.append(speed)
             if speeds:
-                average_speed = sum(speeds) / len(speeds)
+                average_speed = sum(speeds) / len(speeds) #add all speed then divides them by the indexed lengths of all the speeds
                 return f"Average speed: {average_speed:.2f} WPM"
             else:
                 return "No results found."
@@ -63,17 +79,16 @@ class Typeme:
 
     def color_text(self, text, color='white'):
         colors = {
-            'black': '\033[30m',
             'red': '\033[31m',
             'green': '\033[32m',
             'yellow': '\033[33m',
             'blue': '\033[34m',
-            'magenta': '\033[35m',
-            'cyan': '\033[36m',
-            'white': '\033[37m',
         }
         end_color = '\033[0m'
         return f"{colors[color]}{text}{end_color}"
+    
+    def exit():
+        return
 
     
 Typeme()
